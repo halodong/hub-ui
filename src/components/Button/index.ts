1005b7e17a83ab92c24551df2f073cb3ec99afb0
halodong <drl990114@gmail.com>
2022-03-21 20:29:18 +0800

    chore: add btn h-type attribute primary and danger

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,12 +1,26 @@
 export class HubBtn extends HTMLButtonElement {
-  static getClassName (): string {
-    console.log('getClassName')
-    return 'hub-btn'
+  public get btnType (): string {
+    const type = this.getAttribute('h-type')
+    return type == null ? '' : type
+  }
+
+  public set btnType (type: string) {
+    this.setAttribute('h-type', type)
+    // TODO
+    // process className
+  }
+
+  public getClassName (): string {
+    const baseClassNameArr = ['hub-btn']
+    if (this.btnType != null) {
+      baseClassNameArr.push(`btn-${this.btnType}`)
+    }
+    return baseClassNameArr.join(' ')
   }
 
   connectedCallback (): void {
-    console.log('connectedCallback')
-    this.className = HubBtn.getClassName()
+    console.log('connectedCallback', this.getClassName())
+    this.className = this.getClassName()
   }
 }
 
