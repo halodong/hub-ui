8fb699271ef5d059c4fae6231cb5b9dc66b2930f
halodong <drl990114@gmail.com>
2022-03-24 23:59:11 +0800

    chore: fix follow offset

--- src/components/Follow/index.ts
+++ src/components/Follow/index.ts
@@ -14,11 +14,14 @@ export const follow = (
   //   const tarWidth = target.offsetWidth
   //   const tarHeight = target.offsetHeight
   const tirLeft = trigger.offsetLeft
-  const tirTop = trigger.offsetTop
+  let tirTop = trigger.offsetTop
+  // const tirWidth = trigger.offsetWidth
+  const tirHeight = trigger.offsetHeight
 
+  tirTop += tirHeight
   target.style.cssText += `position: absolute;
-  left: Math.round(${tirLeft});
-  top:  Math.round(${tirTop})`
+  left: ${Math.round(tirLeft)}px;
+  top:  ${Math.round(tirTop)}px`
+  console.log(tirLeft, tirTop)
 }
-
-(window as any)._follow = follow
+;(window as any)._follow = follow
