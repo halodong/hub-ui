798a8ad8b243849771510280d17e7131888aa8d5
halodong <drl990114@gmail.com>
2022-03-27 23:08:35 +0800

    chore: add functions and necessary variables for the arrow direction of loop tips

--- src/components/Tips/index.ts
+++ src/components/Tips/index.ts
@@ -7,23 +7,28 @@ export const Tips = (options): void => {
 
   const defaults = {
     eventType: 'hover', // or 'click'
-    content: '',
-    align: 'top center',
+    text: '',
+    position: 'top center',
     delay: 100
   }
   const params = { ...defaults, ...options }
+
+  const { trigger, position, openOn, text } = params
   const el = document.createElement('div')
+  const direction: string = position.split(' ')[0]
+  const offset: string = position.split(' ')[1]
   el.id = 'el'
   const drop = new Drop({
-    target: params.trigger,
-    content: '<a>Welcome to the future</a>',
-    classes: 'drop-theme-arrows',
-    position: 'bottom center',
-    openOn: 'hover'
+    target: trigger,
+    content: `<div class="hub-tips-content">${(text as string)}</div>`,
+    contentClasses: `hub-tips hub-tips-${direction}-${offset}`,
+    position,
+    constrainToWindow: false,
+    constrainToScrollParent: false,
+    openOn
   })
   console.log('tips', drop)
 }
-
 ;(window as any).tips = Tips
 // const positionMap = {
 //   'bottom center': 'top center',
