b52586a27ba5c412c15d992ec41566116afdf16f
halodong <drl990114@gmail.com>
2022-04-05 20:37:34 +0800

    feat: support select disable config

--- src/components/Select/index.ts
+++ src/components/Select/index.ts
@@ -1,3 +1,4 @@
+import { hasClass } from 'utils'
 import { Drop } from 'components/Drop'
 const downArrow = '<svg class="hub-select-icon"width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.34317 7.75732L4.92896 9.17154L12 16.2426L19.0711 9.17157L17.6569 7.75735L12 13.4142L6.34317 7.75732Z" fill="currentColor" /></svg>'
 const upArrow = '<svg class="hub-select-icon"width="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.6569 16.2427L19.0711 14.8285L12.0001 7.75739L4.92896 14.8285L6.34317 16.2427L12.0001 10.5858L17.6569 16.2427Z" fill="currentColor" /></svg>'
@@ -41,8 +42,19 @@ class HubSelect extends HTMLElement {
     return this._onselect
   }
 
+  getItemClassName (data): string {
+    const initial = ['hub-select-item']
+    if (data.disable === true) {
+      initial.push('is-disabled')
+    }
+    if (data.selected === true) {
+      initial.push('is-selected')
+    }
+    return initial.join(' ')
+  }
+
   refresh (): void {
-    const html = this._datasource.map((data) => `<li class="hub-select-item">${(data.label as string)}</li>`)
+    const html = this._datasource.map((data) => `<li class="${this.getItemClassName(data)}">${(data.label as string)}</li>`)
     if (this.drop?.content != null) {
       this.drop.content.innerHTML = `<ul class="hub-select-drop" style="width:${this.input.offsetWidth}px">${html.join('')}</ul>`
     } else {
@@ -70,7 +82,8 @@ class HubSelect extends HTMLElement {
   }
 
   handleSelect = (e): void => {
-    if (e.target == null) return
+    if (e.target.tagName.toLowerCase() !== 'li') return
+    if (hasClass(e.target, 'is-disabled')) return
     this.input.value = (e.target as HTMLElement).innerHTML
     this.selectCallback((e.target as HTMLElement).innerHTML)
     this.drop?.close(e)
