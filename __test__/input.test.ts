157bf5b4f7c810c3645c3a5a75a805e0b18c41a5
halodong <drl990114@gmail.com>
2022-04-30 21:13:13 +0800

    test: Improve the hub-input component test case

--- __test__/input.test.ts
+++ __test__/input.test.ts
@@ -1,11 +1,16 @@
-import { HubInput } from '../src/components/Input'
+import { elHasClassName, renderHubInput } from './testUtils'
 
-const hasBaseClassName = (el: HubInput): boolean => {
-  return el?.className.includes('hub-input')
-}
-
-test('hub-btn has corresponding className', () => {
-  const el = new HubInput()
-  document.body.appendChild(el)
-  expect(hasBaseClassName(el)).toBe(true)
+describe('test Input component', () => {
+  it('should render the correct default input', () => {
+    const el = renderHubInput()
+    expect(el.tagName).toEqual('INPUT')
+    expect(el.disabled).toBeFalsy()
+    expect(elHasClassName(el, 'hub-input')).toBe(true)
+  })
+  it('should render the correct component based on different props', () => {
+    const html = '<input id="base" is="hub-input" inputsize="lg"></input>'
+    document.body.insertAdjacentHTML('beforeend', html)
+    const el = document.getElementById('base')
+    expect(elHasClassName(el, 'input-size-lg')).toBe(true)
+  })
 })
