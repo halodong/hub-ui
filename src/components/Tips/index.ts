7a14f7ee27bc123110a74583ace2d1ca58c3262b
halodong <drl990114@gmail.com>
2022-05-04 02:21:44 +0800

    type: improve the type of the tips component option

--- src/components/Tips/index.ts
+++ src/components/Tips/index.ts
@@ -1,12 +1,12 @@
 import { Drop } from 'components/Drop'
 
-export const Tips = (options): void => {
+export const Tips = (options: TipsOption): HubTips => {
   if (options.trigger == null) {
     throw new Error('Tips Error: You must provide a trigger.')
   }
 
   const defaults = {
-    eventType: 'hover', // or 'click'
+    openOn: 'hover',
     text: '',
     position: 'top center',
     delay: 100
@@ -18,13 +18,45 @@ export const Tips = (options): void => {
   const offset: string = position.split(' ')[1]
   const drop = new Drop({
     target: trigger,
-    content: `<div class="hub-tips-content">${(text as string)}</div>`,
+    content: `<div class="hub-tips-content">${(text)}</div>`,
     contentClasses: `hub-tips hub-tips-${direction}-${offset}`,
     position,
     constrainToWindow: false,
     constrainToScrollParent: false,
     openOn
   })
-  console.log('tips', drop)
+  return {
+    type: 'hub-tips',
+    drop,
+    ...params
+  }
 }
 ;(window as any).tips = Tips
+
+// types
+// --------------------------------------------------------------------------------------------------------------
+interface HubTips extends TipsOption{
+  type: 'hub-tips'
+  drop: Drop
+}
+
+interface TipsOption {
+  trigger: HTMLElement
+  openOn: 'hover'|'click'
+  text: string
+  position: 'top center'
+  | 'bottom left'
+  | 'bottom right'
+  | 'bottom center'
+  | 'top left'
+  | 'top right'
+  | 'top center'
+  | 'top center'
+  | 'right top'
+  | 'right middle'
+  | 'right bottom'
+  | 'left top'
+  | 'left middle'
+  | 'left bottom'
+  delay: number
+}
